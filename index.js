5f94bbbd850dbdf9f07c1821139e9612c0023412
Nidhi842 <124761545+Nidhi842@users.noreply.github.com>
2023-08-27 10:05:40 +0000

    commit

---
+++ index.js
@@ -0,0 +1,10 @@
+console.log("Nidhi");
+const Num = 12345;
+let Name = "Nitu";
+let Age = 12;
+var Add = "pakadi";
+var Add = 
+// Num = 21; // error Because Constant not redecliare again
+Name = "Rekha";
+Age = 23
+console.table([Num, Name, Age]);
